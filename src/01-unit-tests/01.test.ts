8f92e499a4800d1bc109dda43d5be5254e5cfd42
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-03-29 20:25:15 +0300

    second test

--- src/01-unit-tests/01.test.ts
+++ src/01-unit-tests/01.test.ts
@@ -1,4 +1,4 @@
-import {sum} from './01';
+import {mult, sum} from './01';
 
 test ('sum should be correct', () => {
     // data
@@ -20,9 +20,26 @@ test ('sum should be correct', () => {
     const c = 3
 
     // action
-    const result = sum(a, b)
+    const result1 = sum(a, b)
+    const result2 = sum(b, c)
 
     // expect result
-    expect(result).toBe(3)
+    expect(result1).toBe(3)
+    expect(result2).toBe(5)
+})
+
+test ('multiply should be correct', () => {
+    // data
+    const a = 1
+    const b = 2
+    const c = 3
+
+    // action
+    const result1 = mult(a, b)
+    const result2 = mult(b, c)
+
+    // expect result
+    expect(result1).toBe(2)
+    expect(result2).toBe(6)
 })
 
