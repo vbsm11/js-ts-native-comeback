97816b2bba195625aca7bc55cda8381325cc0a8a
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-03-31 22:45:00 +0300

    test completed tasks

--- src/04/04.test.ts
+++ src/04/04.test.ts
@@ -24,3 +24,18 @@ test('should return cheap c', ()=> {
     expect(cheapCourses[0].title).toBe('CSS')
     expect(cheapCourses[1].title).toBe('REACT')
 })
+
+test('get only completed tasks', ()=> {
+    const tasks = [
+        {id: 1, title: 'Bread', isDone: true},
+        {id: 2, title: 'Milk', isDone: false},
+        {id: 3, title: 'Salt', isDone: false},
+        {id: 4, title: 'Sugar', isDone: true}
+    ]
+
+    const completedTasks = tasks.filter(task => task.isDone)
+
+    expect(completedTasks.length).toBe(2)
+    expect(completedTasks[0].id).toBe(1)
+    expect(completedTasks[1].id).toBe(4)
+})
