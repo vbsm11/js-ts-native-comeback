01a1f389247c9eb8223767c313fcd35f7b99b679
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-04-07 20:17:42 +0300

    How callbacks works with onBlur

--- src/06/06-callback.tsx
+++ src/06/06-callback.tsx
@@ -7,23 +7,36 @@
 
 export const User = () => {
 
-  const deleteUser = () => {
-    alert('user have been deleted')
-  }
+    const deleteUser = () => {
+        alert('user have been deleted')
+    }
 
-  const saveUser = () => {
-    alert('user have been saved')
-  }
+    const saveUser = () => {
+        alert('user have been saved')
+    }
 
-  const onNameChanged = () => {
-      console.log('name changed')
-  }
+    const onNameChanged = () => {
+        console.log('name changed')
+    }
 
-  return (
-      <div>
-          <textarea onChange={onNameChanged}>User</textarea>
-        <button onClick={deleteUser}>delete</button>
-        <button onClick={saveUser}>save</button>
-      </div>
-  )
+    const focusLostHandler = () => {
+        console.log('focus lost')
+    }
+    
+    return (
+        <div>
+            <textarea 
+                onChange={onNameChanged}
+                onBlur={focusLostHandler}
+            >
+                User
+            </textarea>
+
+            <input/>
+            <div>
+                <button onClick={deleteUser}>delete</button>
+                <button onClick={saveUser}>save</button>
+            </div>
+        </div>
+    )
 }
