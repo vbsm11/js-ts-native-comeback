7677e0ce3b1087b2b05e33d938d8e318c5a53c9c
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-04-10 00:02:01 +0300

    New properties, example of destructuring with deeper nesting

--- src/07-distructuring-assingment/07.test.ts
+++ src/07-distructuring-assingment/07.test.ts
@@ -2,16 +2,23 @@ test('', () => {
     let props = {
         name: 'Vladimir',
         age: 18,
-        lessons: [{title: '1'}, {title: '2'}]
+        lessons: [{title: '1'}, {title: '2'}],
+        address: {
+            street: {
+                title: 'Lenina'
+            }
+        }
     }
 
     // const age = props.age;
     // const lessons = props.lessons
     const {age, lessons} = props
+    const {title} = props.address.street
 
 
     expect(age).toBe(18)
     expect(lessons.length).toBe(2)
+    expect(title).toBe('Lenina')
 })
 
 
