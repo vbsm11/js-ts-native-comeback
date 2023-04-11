e4f0080439ee16297a0acfc3f21ddbfb8d438d1e
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-04-11 21:48:13 +0300

    rest tests

--- src/09-mutability/09.test.ts
+++ src/09-mutability/09.test.ts
@@ -61,3 +61,66 @@ test('value type test', () => {
 
     expect(usersCount).toBe(100)
 })
+
+test('object in object reference test', () => {
+    const address = {
+        city: 'Seltso'
+    }
+
+    let user: UserType = {
+        name: 'Vladimir',
+        age: 24,
+        address: address
+    }
+
+
+    const user2: UserType = {
+        name: 'Natasha',
+        age: 22,
+        address: address
+    }
+
+    address.city = 'Msk';
+
+    expect(user.address.city).toBe('Msk');
+    expect(user.address.city).toBe(user2.address.city);
+})
+
+test('array in reference test', () => {
+    const address = {
+        city: 'Seltso'
+    }
+
+    const user = {
+        name: 'Vova',
+        age: 24,
+        address: address
+    }
+    const user2 = {
+        name: 'Mama',
+        age: 52,
+        address: address
+    }
+
+    const users = [user, user2, {name: 'Katya', age: 18, address: address}];
+
+    const admins = [user, user2];
+
+    admins[0].name = 'Vladimir'
+
+    expect(users[0].name).toBe('Vladimir')
+})
+
+test('sort array test', () => {
+    const letters = ['c', 'd', 'a', 'z', 'e'];
+
+    pere(letters);
+
+    expect(letters[0]).toBe('c')
+})
+
+function pere(letters: any) {
+    const copy = [...letters];
+    copy.sort();
+    console.log(copy);
+}
