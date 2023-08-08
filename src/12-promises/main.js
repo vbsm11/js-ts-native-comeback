4ba54aa11ddfe552f025614aeff40828291424bd
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-08-08 06:57:44 +0300

    basic promises chain, finally

--- src/12-promises/main.js
+++ src/12-promises/main.js
@@ -2,14 +2,28 @@
 
 const promise1 = axios.get('https://google.com')
 
-// 1ый метод - then
+// 1ый метод - then (promise resolved)
 
 promise1.then((data) => {
     console.log(data)
 })
 
-// 2ой метод - catch
+// 2ой метод - catch (promise rejected)
 
 promise1.catch((error) => {
     console.log(error)
 })
+
+// цепочка промисов
+
+promise1
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+    .finally(() => { // 3ий метод - finally (сработает в любом случае)
+        console.log('finish')
+    })
+
