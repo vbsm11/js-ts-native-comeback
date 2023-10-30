245a1c377b9857fd6058c47b69f73d6595c3907d
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-10-30 06:57:01 +0300

    object with method creating by literal

--- src/13-classes/object.js
+++ src/13-classes/object.js
@@ -13,3 +13,26 @@ const action2 = deleteUser('534543')
 
 console.log(action1)
 console.log(action2)
+
+
+///////////////////////////////////////////////////////////////////////////
+
+function commonHello() {
+    console.log(`I am ${this.name} from ${this.site}`)
+}
+
+const userFabric = (name) => {
+    const user = {
+        name: name,
+        site: 'it-incubator.ru',
+        dateOfBirth: new Date(1998, 2, 1),
+        hello: commonHello
+    }
+    return user
+}
+
+const u1 = userFabric('Vova')
+const u2 = userFabric('Dima')
+
+u1.hello()
+u2.hello()
